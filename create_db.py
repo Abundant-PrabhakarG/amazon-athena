56991f56dd09d488253171f0984ef64329d57aa7
Dr. Prabhakar <47770847+prabhakarg-dm@users.noreply.github.com>
2020-03-01 18:16:37 +0530

    Delete create_db.py

--- create_db.py
+++
@@ -1,9 +0,0 @@
-from pyathena import connect
-
-cursor = connect(aws_access_key_id='AKIAT6ALRAZHH45J6EFH',
-                 aws_secret_access_key='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',
-                 s3_staging_dir='s3://athenatest1032020/test/',
-                 region_name='us-west-2').cursor()
-cursor.execute("CREATE database pythenadb")
-print(cursor.description)
-print(cursor.fetchall())
