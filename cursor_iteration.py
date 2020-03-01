e15d6f51d4899d60562dbc14a08194477d37282b
Dr. Prabhakar <47770847+prabhakarg-dm@users.noreply.github.com>
2020-03-01 18:16:56 +0530

    Delete cursor_iteration.py

--- cursor_iteration.py
+++
@@ -1,9 +0,0 @@
-from pyathena import connect
-
-cursor = connect(aws_access_key_id='AKIAT6ALRAZHH45J6EFH',
-                 aws_secret_access_key='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',
-                 s3_staging_dir='s3://athenatest1032020/bigfile-stage/',
-                 region_name='us-west-2').cursor()
-cursor.execute("SELECT count(*) FROM pythenadb.examplebig limit 1000")
-for row in cursor:
-    print(row)
