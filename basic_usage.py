4d2b8866107891db42806acdab2b111b055679b6
Dr. Prabhakar <47770847+prabhakarg-dm@users.noreply.github.com>
2020-03-01 18:16:24 +0530

    Delete basic_usage.py

--- basic_usage.py
+++
@@ -1,9 +0,0 @@
-from pyathena import connect
-
-cursor = connect(aws_access_key_id='AKIAT6ALRAZHH45J6EFH',
-                 aws_secret_access_key='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',
-                 s3_staging_dir='s3://athenatest1032020/file1/',
-                 region_name='us-west-2').cursor()
-cursor.execute("SELECT * FROM pythenadb.example2")
-print(cursor.description)
-print(cursor.fetchall())
